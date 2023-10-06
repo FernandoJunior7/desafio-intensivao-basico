50f26b9a499dcdc59618add08c9dd3e8bdc91eb5
FernandoJunior7 <135639494+FernandoJunior7@users.noreply.github.com>
2023-10-06 15:12:37 -0300

    desafio concluído 🎉

---
+++ main.js
@@ -0,0 +1,48 @@
+// 01
+alert(`Hello, world!`);
+
+// 02
+let x = 3;
+let y = 2;
+alert(`Soma: ${x + y}`);
+
+// 03
+let numero = 7;
+if (typeof numero === "number") {
+  alert(`Estou imprimindo ${numero}, pois numero é um number.`);
+}
+
+// 04
+let frase = `Olá, mundo!`;
+if (typeof frase === "string") {
+  alert(`Estou imprimindo ${frase}, pois frase é uma string`);
+}
+
+// 05
+let booleano = false;
+if (typeof booleano === "boolean") {
+  alert(`Estou imprimindo ${booleano}, pois booleano é um boolean`);
+}
+
+// 06
+alert(`Subtração: ${x - y}`);
+
+// 07
+alert(`Multiplicação: ${x * y}`);
+
+// 08
+alert(`Divisão: ${x / y}`);
+
+// 09
+if (y % 2 === 0) {
+  alert(`É um número par`);
+} else {
+  alert(`É um número impar`);
+}
+
+// 10
+if (x % 2 !== 0) {
+  alert(`É um número ímpar`);
+} else {
+  alert(`É um número par`);
+}
